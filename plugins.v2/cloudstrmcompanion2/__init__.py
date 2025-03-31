b1cafd0a3e073e36612f307319d6dcd9ddfbe163
LunaticXJ <lalaxjy@gmail.com>
2025-03-31 13:12:40 +0800

    update

--- plugins.v2/cloudstrmcompanion2/__init__.py
+++ plugins.v2/cloudstrmcompanion2/__init__.py
@@ -63,7 +63,7 @@ class CloudStrmCompanion2(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/cloudcompanion.png"
     # 插件版本
-    plugin_version = "1.2.7"
+    plugin_version = "2.0"
     # 插件作者
     plugin_author = "LunaticXJ"
     # 作者主页
--- plugins.v2/cloudstrmcompanion3/__init__.py
+++ plugins.v2/cloudstrmcompanion3/__init__.py
@@ -63,9 +63,9 @@ class CloudStrmCompanion3(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/cloudcompanion.png"
     # 插件版本
-    plugin_version = "1.2.5"
+    plugin_version = "3.0"
     # 插件作者
-    plugin_author = "thsrite"
+    plugin_author = "LunaticXJ"
     # 作者主页
     author_url = "https://github.com/thsrite"
     # 插件配置项ID前缀
