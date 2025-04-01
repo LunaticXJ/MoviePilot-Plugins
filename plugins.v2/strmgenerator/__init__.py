27852a7c996ccf5d20d302df3e5cbccbfe4bafbe
LunaticXJ <lalaxjy@gmail.com>
2025-04-01 18:05:16 +0800

    update

--- plugins.v2/strmgenerator/__init__.py
+++ plugins.v2/strmgenerator/__init__.py
@@ -62,7 +62,7 @@ class StrmGenerator(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/LunaticXJ/MoviePilot-Plugins/main/icons/cloudcompanion.png"
     # 插件版本
-    plugin_version = "1.3"
+    plugin_version = "1.4"
     # 插件作者
     plugin_author = "LunaticXJ"
     # 作者主页
